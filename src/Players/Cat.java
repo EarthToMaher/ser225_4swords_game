9651a10bfd8d1ca9688fb0d49685253b9ad891bd
ChrisF132 <christopherjofalcone@gmail.com>
2025-09-23 17:56:40 -0400

    Fixed collision error

--- src/Players/Cat.java
+++ src/Players/Cat.java
@@ -15,7 +15,7 @@ import java.util.HashMap;
 public class Cat extends Player {
 
     public Cat(float x, float y) {
-        super(new SpriteSheet(ImageLoader.load("megaman4.png"), 538, 538), x, y, "STAND_RIGHT");
+        super(new SpriteSheet(ImageLoader.load("Cat.png"), 24, 24), x, y, "STAND_RIGHT");
         walkSpeed = 2.3f;
     }
 
@@ -32,14 +32,14 @@ public class Cat extends Player {
         return new HashMap<String, Frame[]>() {{
             put("STAND_RIGHT", new Frame[] {
                     new FrameBuilder(spriteSheet.getSprite(0, 0))
-                            .withScale(0.1F)
+                            .withScale(3)
                             .withBounds(6, 12, 12, 7)
                             .build()
             });
 
             put("STAND_LEFT", new Frame[] {
                     new FrameBuilder(spriteSheet.getSprite(0, 0))
-                            .withScale(0.1F)
+                            .withScale(3)
                             .withImageEffect(ImageEffect.FLIP_HORIZONTAL)
                             .withBounds(6, 12, 12, 7)
                             .build()
@@ -47,41 +47,41 @@ public class Cat extends Player {
 
             put("WALK_RIGHT", new Frame[] {
                     new FrameBuilder(spriteSheet.getSprite(1, 0), 14)
-                            .withScale(0.1F)
+                            .withScale(3)
                             .withBounds(6, 12, 12, 7)
                             .build(),
                     new FrameBuilder(spriteSheet.getSprite(1, 1), 14)
-                            .withScale(0.1F)
+                            .withScale(3)
                             .withBounds(6, 12, 12, 7)
                             .build(),
                     new FrameBuilder(spriteSheet.getSprite(1, 2), 14)
-                            .withScale(0.1F)
+                            .withScale(3)
                             .withBounds(6, 12, 12, 7)
                             .build(),
                     new FrameBuilder(spriteSheet.getSprite(1, 3), 14)
-                            .withScale(0.1F)
+                            .withScale(3)
                             .withBounds(6, 12, 12, 7)
                             .build()
             });
 
             put("WALK_LEFT", new Frame[] {
                     new FrameBuilder(spriteSheet.getSprite(1, 0), 14)
-                            .withScale(0.1F)
+                            .withScale(3)
                             .withImageEffect(ImageEffect.FLIP_HORIZONTAL)
                             .withBounds(6, 12, 12, 7)
                             .build(),
                     new FrameBuilder(spriteSheet.getSprite(1, 1), 14)
-                            .withScale(0.1F)
+                            .withScale(3)
                             .withImageEffect(ImageEffect.FLIP_HORIZONTAL)
                             .withBounds(6, 12, 12, 7)
                             .build(),
                     new FrameBuilder(spriteSheet.getSprite(1, 2), 14)
-                            .withScale(0.1F)
+                            .withScale(3)
                             .withImageEffect(ImageEffect.FLIP_HORIZONTAL)
                             .withBounds(6, 12, 12, 7)
                             .build(),
                     new FrameBuilder(spriteSheet.getSprite(1, 3), 14)
-                            .withScale(0.1F)
+                            .withScale(3)
                             .withImageEffect(ImageEffect.FLIP_HORIZONTAL)
                             .withBounds(6, 12, 12, 7)
                             .build()
@@ -89,3 +89,4 @@ public class Cat extends Player {
         }};
     }
 }
+
